36e2590ffd3b428baa3fe52212283e8eff598604
cdi114 <cdi114@pltz859.AFPA-PARIS-DI>
2017-10-24 15:29:44 +0200

    2e version bonus 05

--- exercices-algo/src/exosSupplementaires/bonus05.java
+++ exercices-algo/src/exosSupplementaires/bonus05.java
@@ -11,27 +11,27 @@ package exosSupplementaires;
 
 public class bonus05 {
 
-    public static void main(String[] args){
-       
+    public static void main(String[] args) {
+
         String debut;
         String ligne;
+        int nblignes = 300;
 
-        for (int i = 1; i <= 500; i++) {
-            
+        for (int i = 1; i <= nblignes; i++) {
+            ligne = "";
             if (i % 2 == 1) {         // rang impair
-                debut = "ox";
-                ligne = debut;
-            } else {
-                debut = "xo";
-                ligne = debut;
+                debut = "OX";
+
+            } else {                    // rang pair
+                debut = "XO";
             }
 
-            for (int j = 1; j <= 500; j++) {
+            for (int j = 2; j <= nblignes / 2; j++) {
                 ligne = ligne + debut;
-
             }
+
             System.out.println(ligne);
         }
-      
+
     }
 }

aeea12829667d5b27c1b3ec0144b8ffd9e12c2a8
cdi114 <cdi114@pltz859.AFPA-PARIS-DI>
2017-10-24 17:15:34 +0200

    fin de journée

--- intro-Java/src/homework/FrancoisTesteLeJava.java
+++ intro-Java/src/homework/FrancoisTesteLeJava.java
@@ -1,14 +1,24 @@
-
 package homework;
 
-/**
- *
- * @author François Séguin
- */
 public class FrancoisTesteLeJava {
+
     public static void main(String[] args) {
-        String nom = "François";
-        System.out.println("Bonjour " + nom);
-                
+
+//        float prixbillets[];
+//        prixbillets = new float[3];
+//        float prixbillets[] = new float[3
+        double prixbillets[] = {153.01, 153.01, 139.01};
+
+        String passagers[] = {"Patricia", "Lilou", "Tom"};
+
+        double somme;
+        int i;
+
+        for (i = 0, somme = 0; i < passagers.length; i++) {
+            somme += prixbillets[i];
+            System.out.println("Le billet de " + passagers[i] + "\t coûte " + prixbillets[i] + "€");
+
+        }
+        System.out.println("\nTotal pour la famille:" + "\t>> " + somme + "€ <<");
     }
 }
